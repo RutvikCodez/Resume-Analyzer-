a3cd87ab4a04ea90671ed37dd8e2a43bf1a550ea
RutvikCodez <rutvikdarji21@gnu.ac.in>
2026-01-28 15:00:44 +0530

    implemented authentication functionality using clerk

--- components/CTAButton.tsx
+++ components/CTAButton.tsx
@@ -1,15 +1,20 @@
+import { SignedOut, SignUpButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 import { ArrowRight } from "lucide-react";
 
 const CTAButton = () => {
   return (
-    <Button
-      size="lg"
-      className="bg-primary hover:bg-primary/90 text-primary-foreground rounded-full px-8 py-6 text-lg font-semibold flex gap-2"
-    >
-      Analyze Resume Free
-      <ArrowRight />
-    </Button>
+    <SignedOut>
+      <SignUpButton>
+        <Button
+          size="lg"
+          className="bg-primary hover:bg-primary/90 text-primary-foreground rounded-full px-8 py-6 text-lg font-semibold flex gap-2"
+        >
+          Analyze Resume Free
+          <ArrowRight />
+        </Button>
+      </SignUpButton>
+    </SignedOut>
   );
 };
 
