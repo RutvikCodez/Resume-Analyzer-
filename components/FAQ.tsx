88991f62018db86cd2baf529b66cdcf82421403a
RutvikCodez <rutvikdarji21@gnu.ac.in>
2026-01-28 14:16:14 +0530

    removed unused comment

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -4,21 +4,6 @@ import SecondaryWrapper from "./SecondaryWrapper";
 
 const FAQ = () => {
   return (
-    // <section className="w-full py-20 bg-secondary/30">
-    //   <div className="max-w-3xl mx-auto flex flex-col gap-20">
-    //     <div className="text-center">
-    //       <h2 className="text-4xl md:text-5xl font-bold text-pretty">
-    //         Frequently Asked Questions
-    //       </h2>
-    //     </div>
-
-    // <div className="flex flex-col gap-4">
-    //   {faqs.map((faq, idx) => (
-    //     <ProblemCard key={idx} {...faq} />
-    //   ))}
-    // </div>
-    //   </div>
-    // </section>
     <SecondaryWrapper title="Frequently Asked Questions" desc="">
       <div className="flex flex-col gap-4">
         {faqs.map((faq, idx) => (
--- components/Platform.tsx
+++ components/Platform.tsx
@@ -4,24 +4,6 @@ import SecondaryWrapper from "./SecondaryWrapper";
 
 const Platform = () => {
   return (
-    // <section className="w-full py-20 bg-linear-to-br from-accent/5 to-primary/5">
-    //   <div className="max-w-6xl mx-auto flex flex-col gap-16">
-    //     <div className="text-center flex flex-col gap-4">
-    //       <h2 className="text-4xl md:text-5xl font-bold text-pretty">
-    //         Your Dashboard
-    //       </h2>
-    //       <p className="text-xl text-muted-foreground">
-    //         Everything you need in one place
-    //       </p>
-    //     </div>
-
-    // <div className="grid md:grid-cols-3 gap-6">
-    //   {platformData.map((feature, idx) => (
-    //     <ProblemCard key={idx} {...feature} />
-    //   ))}
-    // </div>
-    //   </div>
-    // </section>
     <SecondaryWrapper
       title="Your Dashboard"
       desc="Everything you need in one place"
